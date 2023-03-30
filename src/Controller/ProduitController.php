53f2cf17fc9dd59fa6f8687ddba6c88c4d659dcd
jlb86340 <math86340@orange.fr>
2023-03-30 12:20:05 +0200

    ajout des layout listing view et de tt les methodes dans le controleurs

--- src/Controller/ProduitController.php
+++ src/Controller/ProduitController.php
@@ -10,20 +10,20 @@ use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/produit', name: 'produit')]
 class ProduitController extends AbstractController
 {
-    #[Route('/produit', name: 'app_produit')]
-    public function index(): JsonResponse
+
+    #[Route('', name: '')]
+    public function indexAction(): Response
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/ProduitController.php',
-        ]);
+        return $this->redirectToRoute('produit_list');
     }
 
-    #[Route('/produit/add', name: 'add_produit')]
+    #[Route('/add', name: '_add')]
     public function produitAddAction(EntityManagerInterface $em, Request $request): Response
     {
         $product = new Product();
@@ -50,5 +50,57 @@ class ProduitController extends AbstractController
 
 
         return $this->render('Form/add_product.html.twig', $args);
+    }
+    #[Route('/list', name: '_list')]
+    public function listAction(EntityManagerInterface $em): Response
+    {
+        $productRepository = $em->getRepository(Product::class);
+        $products = $productRepository->findAll();
+        $args = array(
+            'produits' => $products,
+        );
+        return $this->render('Product/listing.html.twig', $args);
+    }
+
+    #[Route(
+        '/view/{id}',
+        name: '_view',
+        requirements: ['id' => '[1-9]\d*'],
+    )]
+    public function viewAction(int $id, EntityManagerInterface $em): Response
+    {
+        $productRepository = $em->getRepository(Product::class);
+        $product = $productRepository->find($id);
+
+        if (is_null($product))
+        {
+            $this->addFlash('info', 'view : product ' . $id . ' inexistant');
+            return $this->redirectToRoute('produit_list');
+        }
+
+        $args = array(
+            'produit' => $product,
+        );
+        return $this->render('Product/view.html.twig', $args);
+    }
+
+    #[Route(
+        '/delete/{id}',
+        name: '_delete',
+        requirements: ['id' => '[1-9]\d*'],
+    )]
+    public function deleteAction(int $id, EntityManagerInterface $em): Response
+    {
+        $productRepository = $em->getRepository(Product::class);
+        $product = $productRepository->find($id);
+
+        if (is_null($product))
+            throw new NotFoundHttpException('erreur suppression produit ' . $id);
+
+        $em->remove($product);
+        $em->flush();
+        $this->addFlash('info', 'suppression produit ' . $id . ' réussie');
+
+        return $this->redirectToRoute('produit_list');
     }
 }
--- src/Entity/Order.php
+++ src/Entity/Order.php
@@ -17,11 +17,24 @@ class Order
     #[ORM\Column]
     private ?int $quantity = null;
 
+    #[ORM\ManyToOne(targetEntity: User::class)]
+    #[ORM\JoinColumn(
+        name:'id_user',
+        nullable: false
+    )]
+    private ?User $user = null;
+
+    #[ORM\ManyToOne(targetEntity: Product::class)]
+    #[ORM\JoinColumn(
+        name: 'id_product',
+        nullable: false
+    )]
+    private ?Product $product = null;
+
     public function getId(): ?int
     {
         return $this->id;
     }
-
     public function getQuantity(): ?int
     {
         return $this->quantity;
@@ -30,6 +43,30 @@ class Order
     public function setQuantity(int $quantity): self
     {
         $this->quantity = $quantity;
+
+        return $this;
+    }
+
+    public function getUser(): ?User
+    {
+        return $this->user;
+    }
+
+    public function setUser(?User $user): self
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+
+    public function getProduct(): ?Product
+    {
+        return $this->product;
+    }
+
+    public function setProduct(?Product $product): self
+    {
+        $this->product = $product;
 
         return $this;
     }
