8313e07d0a175fc44324be317760f5d680e677aa
Vaingane <85734569+Vaingane@users.noreply.github.com>
2023-04-06 14:46:03 +0200

    Ajout de contrôle d'authentification

--- src/Controller/MagasinController.php
+++ src/Controller/MagasinController.php
@@ -2,15 +2,32 @@
 
 namespace App\Controller;
 
+use App\Entity\Product;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/magasin', name: 'magasin')]
 class MagasinController extends AbstractController
 {
-    #[Route('/magasin', name: 'app_magasin')]
-    public function loginAction(): Response
+    #[Route('', name: '')]
+    public function indexAction(): Response
     {
-        return $this->render('Security/login.html.twig');
+        return $this->redirectToRoute('magasin_stock');
     }
+
+    #[Route('/stock', name: '_stock')]
+    public function stockAction(EntityManagerInterface $em): Response
+    {
+        $productRepository = $em->getRepository(Product::class);
+        $products = $productRepository->findAll();
+        $args = array(
+            'produits' => $products,
+        );
+        return $this->render('Shop/stock.html.twig', $args);
+    }
+
+
 }
